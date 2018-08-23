b124fa00312a592f7e733bb64b97cb8f8ff127bb
Thomas Rice <thomas.rice@asos.com>
2018-08-23 16:27:11 +0100

    tests

---
+++ API-With-Read-Functionality/src/TestObject.cs
@@ -0,0 +1,14 @@
+﻿namespace loadtesting
+{
+    public class TestObject
+    {
+        public string someAttribute { get; set; }
+        public int someOtherAttribute { get; set; }
+        public string id { get; set; }
+        public string _rid { get; set; }
+        public string _self { get; set; }
+        public string _etag { get; set; }
+        public string _attachments { get; set; }
+        public int _ts { get; set; }
+    }
+}
