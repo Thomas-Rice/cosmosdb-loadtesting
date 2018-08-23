1f699639a2c2519849d66f296d7ceaed141a7792
Thomas Rice <thomas.rice@asos.com>
2018-08-23 14:17:38 +0100

    added csv parsing

--- src/Controllers/LoadController.cs
+++ src/Controllers/LoadController.cs
@@ -1,5 +1,8 @@
-﻿using System.Linq;
+﻿using System.Globalization;
+using System.IO;
+using System.Linq;
 using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
 using Newtonsoft.Json;
 
 namespace loadtesting.Controllers
@@ -10,37 +13,61 @@ namespace loadtesting.Controllers
 
     public class LoadController : Controller
     {
+        private const string RequestChargeTextFile = @"C:\Users\thomas.rice\Documents\LoadTestResults\RequestCharge.txt";
+        private const string ResponseTimeTextFile = @"C:\Users\thomas.rice\Documents\LoadTestResults\ResponseTime.txt";
+        private const string OutputCsv = @"C:\Users\thomas.rice\Documents\LoadTestResults\RequestChargeCSV.csv";
         private readonly IDocumentDbService _documentDbService;
-        private readonly ITestDocumentService _testDocumentService;
-        private DocumentClient _client;
+        private readonly DocumentClient _client;
 
         public LoadController(IDocumentDbService documentDbService, ITestDocumentService testDocumentService)
         {
             _documentDbService = documentDbService;
-            _testDocumentService = testDocumentService;
             _client = _documentDbService.ReturnClient();
 
         }
 
         [ActionName("Write")]
-        public async Task<ActionResult> CreateAsync()
+        public ActionResult Create()
         {
-            await _documentDbService.AddItemAsync(_testDocumentService.GetDocument());
+            var responseTimes = System.IO.File.ReadAllLines(ResponseTimeTextFile).FirstOrDefault().Split(',').ToList();
+            var requestCharges = System.IO.File.ReadAllLines(RequestChargeTextFile).FirstOrDefault().Split(',').ToList();
+
+            using (TextWriter sw = new StreamWriter(OutputCsv))
+            {
+                for (var i = 0; i < requestCharges.Count; i++)
+                {
+                    sw.WriteLine("{0}, {1}", responseTimes[i], requestCharges[i]);
+                }
+            }
             return Ok();
         }
 
+
         [ActionName("Read")]
-        public string Read()
+        public async Task<string> Read()
         {
             var query = "SELECT * FROM c WHERE c.availableFrom <= 1534946233 AND c.availableTo >= 1534946233 AND" +
                         " c.warehouse = 'FC01' AND c.serviceLevel = 'Standard Delivery' " +
                         "AND (c.deliveryZone = 'GBR' OR c.deliveryZone = 'GBR\\England' OR c.deliveryZone = 'GBR\\England\\London')";
-            //var query = "SELECT * FROM c WHERE c.id = \"toRead\"";
 
             var link = UriFactory.CreateDocumentCollectionUri("ReadTest", "Data").ToString();
-            var test = _client.CreateDocumentQuery<CalendarEntry>(link, query).AsEnumerable().ToList();
-            var test2 = JsonConvert.SerializeObject(test, Formatting.Indented);
-            return test2;
+            var test = _client.CreateDocumentQuery<CalendarEntry>(link, query, new FeedOptions
+            {
+                PopulateQueryMetrics = true,
+            }).AsDocumentQuery();
+
+            var result = await test.ExecuteNextAsync();
+            var metrics = result.QueryMetrics;
+            var requestCharge = result.RequestCharge;
+            var totalMilliseconds = metrics.FirstOrDefault().Value.TotalTime.TotalMilliseconds.ToString(CultureInfo.InvariantCulture);
+
+            System.IO.File.AppendAllText(ResponseTimeTextFile, totalMilliseconds + ",");
+            System.IO.File.AppendAllText(RequestChargeTextFile, requestCharge + ",");
+
+            var jsonResponseForDisplay = JsonConvert.SerializeObject(test, Formatting.Indented);
+            return jsonResponseForDisplay;
         }
+
+
     }
 }
